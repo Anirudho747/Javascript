2e74da053974e6c94914fd300d59f47e36cca3b4
Ani <anirudho747@gmail.com>
2024-03-28 13:04:49 +0530

    first lessons

---
+++ DataType.js
@@ -0,0 +1,30 @@
+let fname="Ani"
+console.log(fname)
+console.log(typeof fname)
+
+let marks = 98
+console.log(typeof marks)
+console.log(marks)
+
+let status =true
+console.log(typeof status)
+console.log(status)
+
+let lname
+console.log(typeof lname)
+console.log(lname);
+
+let address = null
+console.log(typeof address)
+console.log(address);
+
+let person = {
+
+    "Fname" : "Ani",
+    "Lname" : "Sengupta",
+    "Age":34,
+    "Divorced":true
+}
+
+console.log(typeof person);
+console.log(person)
--- Hello3.js
+++ Hello3.js
@@ -1,3 +1,6 @@
 console.log("Joy Maa Kaali")
 console.log(7)
 console.error("Something went wrong")
+console.error("Show a Warning")
+console.info("This is infor")
+
