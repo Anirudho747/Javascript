91437840178697b08dc942683efd5b3042113901
Ani <anirudho747@gmail.com>
2024-04-02 06:33:14 +0530

    Var,Let,Const demo

---
+++ varLetConstDemo.js
@@ -0,0 +1,25 @@
+var fname="Ani";
+var fname="Sengupta";
+console.log(fname);
+
+let lname="Anir";
+ lname="ASengupta";
+console.log(lname);
+
+const mname="Anir";
+//mname="AGupta";
+console.log(lname);
+
+for(var i=0;i<5;i++)
+{
+    console.log(i);
+}
+
+console.log("Outside value is ",i);
+
+for(let x=0;x<5;x++)
+{
+    console.log(x);
+}
+
+console.log("Outside value is ",x);
